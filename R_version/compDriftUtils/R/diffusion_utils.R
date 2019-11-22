113f72f0b06288e98d53fdb6ddaf6f5343505041
DESKTOP-82209V4\ammon_work <ammonthompson@gmail.com>
2019-11-22 14:04:32 -0800

    adapted expression drift for R. located in R_version directory

---
+++ R_version/compDriftUtils/R/diffusion_utils.R
@@ -0,0 +1,142 @@
+#' Generate random variate from density function for D.
+#'
+#' See equations 5 and 6 in Thompson et al. 2016. Genetics
+#' @param sig2 is the diffusion rate parameters. Equation 4 Thompson et al. 2016.
+random_D <- function(sigmaD2 = 1, D0 = 0, time = 10^4, Dstar = 10, num_sample = 100){
+  D_sample <- c()
+  for(i in 1:num_sample){
+    missed <- TRUE;
+    isNeo <- FALSE;
+    t <- time;
+    diffusionRate <- sigmaD2;
+    sigma2Dt <- diffusionRate * t;
+    D <- D0;
+
+    #determine if one of the paralogs neofuncitonalized (touched one of the boundaries).
+    pNeoL <- pNeo_at_RorL(Dstar, D, diffusionRate, t, "L")
+    pNeoR <- pNeo_at_RorL(Dstar, D, diffusionRate, t, "R")
+    pNeo <- pNeoR + pNeoL
+
+    if((Dstar-abs(D)) <= 0) isNeo<-TRUE
+    if(runif(1) <= pNeo && !isNeo){
+      if(runif(1) <= pNeoL/pNeo){
+        D <- -Dstar;
+      }else{
+        D <- Dstar;
+      }
+      isNeo <- TRUE;
+    }
+
+    #Draw a random value for D(t) from N(D,sigmaD2t) if neofuncitonalization is virtually impossible (to speed up calculation)
+    #Draw a random value for D(t) if there is a signficant prob of neo, but neo did not occur
+    #Accept-reject algorithm: algorithm works by drawing a box around a the distribution left to right and zero to 2*maxprob.
+    #Draw random pairs of d and % of 2*maximum from this box to evaluate its relative probability and use that to determine if
+    #that value for d hit or missed.
+    if(pNeo < 0.0001 && !isNeo){
+
+      D <- D + rnorm(1)*sqrt(sigma2Dt);
+      if(D < -Dstar){D <- -Dstar;}
+      if(D > Dstar){D <- Dstar;}
+
+    }else if(!isNeo){
+
+      #Set the left and right boundaries as 4 standard deviations from the mean (D) or the absorbing boundaries if greater than.
+      if(-4*sqrt(sigma2Dt)+D > -Dstar) left <- D-4*sqrt(sigma2Dt) else left <- -Dstar
+      if(4*sqrt(sigma2Dt)+D < Dstar) right <- D+4*sqrt(sigma2Dt) else right <- Dstar
+
+      maxd <- maxFinder(D, sigma2Dt, Dstar, left, right);
+      #rejection sampling
+      while(missed){
+        imagesMaxd<-0;
+        imagesd<-0;
+        d<-runif(1)*(right-left) + left;
+        nMax<-((sqrt(36.8*sigma2Dt)-(d+D))/(4.0*Dstar))+1;
+        for(n in 0:(nMax-1)){
+          imagesd <- imagesd -
+            exp(-(1/(2*sigma2Dt)*(d-(-D+(4*n+2)*Dstar))^2))+
+            exp(-(1/(2*sigma2Dt)*(d-(D-(4*n+4)*Dstar))^2))-
+            exp(-(1/(2*sigma2Dt)*(d-(-D-(4*n+2)*Dstar))^2))+
+            exp(-(1/(2*sigma2Dt)*(d-(D+(4*n+4)*Dstar))^2));
+          imagesMaxd <- imagesMaxd -
+            exp(-(1/(2*sigma2Dt)*(maxd-(-D+(4*n+2)*Dstar))^2))+
+            exp(-(1/(2*sigma2Dt)*(maxd-(D-(4*n+4)*Dstar))^2))-
+            exp(-(1/(2*sigma2Dt)*(maxd-(-D-(4*n+2)*Dstar))^2))+
+            exp(-(1/(2*sigma2Dt)*(maxd-(D+(4*n+4)*Dstar))^2));
+        }
+
+        pdfnearDmax <- (1/sqrt(2.0*pi*sigma2Dt))*(exp(-(1/(2.0*sigma2Dt))*(maxd-D)^2)+imagesMaxd);
+        pdfatd <- (1/sqrt(2.0*pi*sigma2Dt))*(exp(-(1/(2.0*sigma2Dt))*(d-D)^2)+imagesd);
+        randProb <- runif(1)*pdfnearDmax*2;
+        if(pdfnearDmax <= 0 ){
+          pdfnearDmax<-1;
+          pdfatd<-1
+        }
+        if(pdfatd <= 0) pdfatd <- pdfnearDmax
+        if(randProb < pdfatd){
+          missed<-FALSE;
+          D <- d;
+        }
+      }
+    }
+    D_sample <- c(D_sample, D)
+  }
+  return( D_sample );
+
+}
+
+maxFinder <- function(D, sigma2t, Dthresh, start, finish){
+  pdfatd=0;
+  d=start;
+  imD = 0;
+  nMax = D;
+  increment = (finish - start)/100.0;
+  for(i in 1:100){
+    d =start + i*increment;
+    imD=0;
+    nMax=((sqrt(36.8*sigma2t)-(d+D))/(4.0*Dthresh))+1;
+    for(n in 0:(nMax-1)){
+      imD = imD -
+        exp(-(1/(2*sigma2t)*(d-(-D+(4*n+2)*Dthresh))^2))+
+        exp(-(1/(2*sigma2t)*(d-(D-(4*n+4)*Dthresh))^2))-
+        exp(-(1/(2*sigma2t)*(d-(-D-(4*n+2)*Dthresh))^2))+
+        exp(-(1/(2*sigma2t)*(d-(D+(4*n+4)*Dthresh))^2));
+    }
+    if(pdfatd > ((1/sqrt(2.0*pi*sigma2t))*(exp(-(1/(2.0*sigma2t))*(d-D)^2)+imD))){
+      break
+    }else{
+      pdfatd = (1/sqrt(2.0*pi*sigma2t))*(exp(-(1/(2.0*sigma2t))*(d-D)^2)+imD)
+    }
+  }
+  return( d+0.5*increment);
+}
+
+pNeo_at_RorL = function(xDstar, xD0, xsigmaD2, xtime, R_L){
+
+  #determine prob of touched Dstar.
+  pNeo=0
+  neoMaxIndex = (5.0*sqrt(xsigmaD2 * xtime)-xDstar)/(4.0*xDstar)+2;
+
+  if(R_L == "R"){
+
+    for(n in 0:neoMaxIndex){
+
+      pNeo = pNeo + 2*(pnorm(((4*n+3)*xDstar+xD0)/sqrt(xsigmaD2 * xtime)) -
+                         pnorm(((4*n+1)*xDstar-xD0)/sqrt(xsigmaD2 * xtime)))
+    }
+
+  }else{
+
+    for(n in 0:neoMaxIndex){
+
+      pNeo = pNeo + 2*(pnorm(((4*n+3)*xDstar-xD0)/sqrt(xsigmaD2 * xtime)) -
+                         pnorm(((4*n+1)*xDstar+xD0)/sqrt(xsigmaD2 * xtime)))
+
+
+    }
+
+  }
+
+  return(pNeo)
+
+}
+
---
+++ R_version/compDriftUtils/R/discrete_utils.R
@@ -0,0 +1,89 @@
+#' Simulate two paralogs evolving by compensatory drift.
+#'
+#' @param trace will plot out the simulations sampled every sample_rate mutations
+
+run_stochastic_sim <- function(p1_start = 5, p2_start = 5, theta = 10, sim_time = 10^4,
+                             mu = 10^-2, N = 10^2, sigma_m = 1, omega = 2,
+                             trace = FALSE, sample_rate = 10000, num_sample = 1 ){
+  p1_p2 = c()
+  for(i in 1:num_sample){
+    time <- 0
+    p1_0 <- p1_start
+    p2_0 <- p2_start
+    pp <- 0.5
+    if(trace) plot(NULL, xlim = c(0, sim_time), ylim = c(0, 1.1 * theta))
+    num_mutations <- 0
+    time_trace <- vector("numeric", 10000)
+    p1_trace <- vector("numeric", 10000)
+    p2_trace <- vector("numeric", 10000)
+    index <- 1
+    while(time < sim_time){
+
+      ## time of mutation if one or neither is absorbed (abs(p1 - p2) > theta)
+      time <- time + rexp(1, rate = 4 * N * mu) #pop. mut. rate for 2 diploid loci; 4N
+      if(p1_0 - p2_0 > theta){
+        pp <- 1
+        p2_0 <- 0
+        p1_0 <- theta
+        break
+      }else if(p1_0 - p2_0 < -theta){
+        pp <- 0
+        p1_0 <- 0
+        p2_0 <- theta
+        break
+      }
+
+      if(time > sim_time) break
+
+      p1mut <- p1_0
+      p2mut <- p2_0
+
+      if(runif(1) < pp) p1mut <- rnorm(1, p1_0, sigma_m) else p2mut <- rnorm(1, p2_0, sigma_m)
+      num_mutations <- num_mutations + 1
+
+      if(runif(1) < pfix(p1_0, p2_0, p1mut, p2mut, theta, omega, N)){
+        p1_0 <- p1mut
+        p2_0 <- p2mut
+      }
+
+      ### store p1 and p2 every X generations
+      if((num_mutations %% sample_rate) == 0 & trace){
+        lines(cbind(c(time, time_trace[index-1]),
+                    c(p1_0, p1_trace[index-1]) ), col = "red")
+        lines(cbind(c(time, time_trace[index-1]),
+                    c(p2_0, p2_trace[index-1]) ), col = "blue")
+
+        Sys.sleep(0.02)
+        time_trace[index] <- time
+        p1_trace[index] <- p1_0
+        p2_trace[index] <- p2_0
+        index <- index + 1
+      }
+    }#end simulation
+    p1_p2 <- rbind(p1_p2, c(p1_0, p2_0))
+  }
+
+  colnames(p1_p2) <- c("p1", "p2")
+  return(p1_p2)
+}
+
+compDrift_var = function(tt){
+  return(tt * 1.543 * (mu * omega^3)/(sigma_m * N^(3/2)))
+}
+
+fitness = function(xp1, xp2, theta, omega){
+  return(exp(-(xp1 + xp2 - theta)^2/(2 * omega^2)))
+}
+
+pfix = function(xp1, xp2, xp1_m, xp2_m, theta, omega, N){
+
+  current_fitness = fitness(xp1, xp2, theta, omega)
+  mut_fitness = fitness(xp1_m, xp2_m, theta, omega)
+  s = mut_fitness/current_fitness - 1
+  if(is.nan(s)) s = -1
+  fix_prob = (1 - exp(-2 * s))/(1 - exp(-4 * N * s))
+  if(s == 0) fix_prob = 1/(2 * N)
+  return(fix_prob)
+
+}
+
